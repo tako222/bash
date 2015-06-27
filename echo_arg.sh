1536eaae1c3df661cb8e2c584faa42af0d1f7cb7
tako222 <throwing_over_one_s_shoulder@yahoo.co.jp>
2015-06-27 21:35:27 +0900

    Echo arguments

---
+++ echo_arg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Echo all arguments
+
+# Set arguments count
+ARG_COUNT=$#
+if [ $ARG_COUNT -eq 0 ]; then 
+  echo "argument is zero"
+  exit 1
+fi
+
+# Echo all
+COUNT=1
+while [ $COUNT -le $ARG_COUNT ]
+do
+  echo -n "[$1]"
+  if [ `expr $COUNT % 10` -eq 0 ]; then
+    echo 
+  fi
+  shift
+  COUNT=`expr $COUNT + 1`
+done
+
+echo 
